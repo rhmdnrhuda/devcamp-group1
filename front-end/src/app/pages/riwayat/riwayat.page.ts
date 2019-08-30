78d387831746298e144832dc024baa5bede07de2
Nakama <nakama-203656@203656.local>
2019-08-30 21:19:24 +0700

    date report from API

--- front-end/src/app/pages/riwayat/riwayat.page.ts
+++ front-end/src/app/pages/riwayat/riwayat.page.ts
@@ -1,69 +1,33 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, AfterViewInit, OnInit } from '@angular/core';
+import { ChatbotService } from '../../services/chatbot.service';
+import { compareAsc, format } from 'date-fns'
 
 @Component({
   selector: 'app-riwayat',
   templateUrl: './riwayat.page.html',
   styleUrls: ['./riwayat.page.scss'],
 })
-export class RiwayatPage implements OnInit {
-  balance: Number = 90000000;
-  dateNow;
-  dateFirstThisMonth;
+export class RiwayatPage implements OnInit, AfterViewInit {
+  balance: Number;
+  startDate: any;
+  endDate: any;
 
-  data = [
-    {
-        "data": [
-            {
-                "Category": "Food",
-                "Amount": 20000000,
-                "Type": "Expense",
-                "Timestamp": "2019-08-30 13:52:50.0",
-                "Note": " "
-            },
-            {
-                "Category": "Food",
-                "Amount": 200000,
-                "Type": "Income",
-                "Timestamp": "2019-08-30 12:03:59.0",
-                "Note": " "
-            }
-        ],
-        "tanggal": "2019-08-30",
-        "total": 2000900,
-    },
-    {
-        "data": [
-            {
-                "Category": "Food",
-                "Amount": 200000,
-                "Type": "Expense",
-                "Timestamp": "2019-08-29 21:49:39.0",
-                "Note": " "
-            },
-            {
-                "Category": "Food",
-                "Amount": 200000,
-                "Type": "Expense",
-                "Timestamp": "2019-08-29 21:12:17.0",
-                "Note": " "
-            }
-        ],
-        "tanggal": "2019-08-29",
-        "total": -2000900,
-    }
-];
-
-  constructor() { }
-
-  ngOnInit() {
-    this.setDate();
+  
+  constructor(private chatbotService : ChatbotService) { 
   }
 
-  setDate(){
-    this.dateNow = Date.now();
+  ngAfterViewInit(){
+  }
 
-    console.log(this.dateNow)
+  ngOnInit() {
+    this.chatbotService.getBalance();
+  }
+
+  setFormatDate(dateTime){
+    dateTime = new Date(dateTime);
+    dateTime = dateTime.toISOString().slice(0,10);
+    
+    return dateTime;
   }
 
 }
