8ace840f22631e6d17c65be7abd9af83ee88adea
Cifran Hiu <cifranhiu21@gmail.com>
2019-08-30 17:15:20 +0700

    update /getTransactions - loop mundur, json lebih rapi

--- Backend/WalletBotDevCamp/src/walletbotdevcamp/httphandler/GetTransactionHandler.java
+++ Backend/WalletBotDevCamp/src/walletbotdevcamp/httphandler/GetTransactionHandler.java
@@ -6,6 +6,7 @@
 package walletbotdevcamp.httphandler;
 
 import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.sun.net.httpserver.HttpExchange;
@@ -14,6 +15,7 @@ import java.io.IOException;
 import java.sql.Timestamp;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.Map;
 import java.util.logging.Level;
@@ -26,9 +28,11 @@ import walletbotdevcamp.processor.ChatProcessor;
  * @author cifran
  */
 public class GetTransactionHandler extends SimpleHttpHandler {
+    private SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+            
     @Override
     public String processRequest(int requestIdentifier, String fullRequestBody) {
-        JsonArray array = new JsonArray();
+        JsonArray result = new JsonArray();
         try {
             Map<String, String> params = queryToMap(fullRequestBody);
             
@@ -36,16 +40,84 @@ public class GetTransactionHandler extends SimpleHttpHandler {
             String startDate = params.get("startdate");
             String endDate = params.get("enddate");
             
-            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
             Date dateStartDate = sdf.parse(startDate);
             Date dateEndDate = sdf.parse(endDate);
-            Timestamp timestampStartDate = new Timestamp(dateStartDate.getTime());
-            Timestamp timestampEndDate = new Timestamp(dateEndDate.getTime());
             
-            array = new Database().getTransactions(userId, timestampStartDate, timestampEndDate);
+//            // looping per day
+//            Date current = dateStartDate;
+//            while(!current.after(dateEndDate)) {
+//                Date nextDate = getPrevDate(current);
+//                
+//                JsonObject dailyData = fetchData(userId, current, nextDate);
+//                result.add(dailyData);
+//                
+//                current = nextDate;
+//            }
+            Date current = dateEndDate;
+            while(!current.before(dateStartDate)) {
+                Date prevDate = getPrevDate(current);
+                
+                JsonObject dailyData = fetchData(userId, current, getNextDate(current));
+                if (dailyData != null) {
+                    result.add(dailyData);
+                }
+                
+                current = prevDate;
+            }
         } catch (Exception ex) {
             logger.error(ex);
         }
-        return array.toString();
+        return result.toString();
+    }
+    
+    private Date getNextDate(Date current) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(current);
+        calendar.add(Calendar.DATE, 1);
+        return calendar.getTime();
+    }
+    
+    private Date getPrevDate(Date current) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(current);
+        calendar.add(Calendar.DATE, -1);
+        return calendar.getTime();
+    }
+    
+    private JsonObject fetchData(String userId, Date current, Date nextDate) {
+        Timestamp currTime = new Timestamp(current.getTime());
+        Timestamp nextTime = new Timestamp(nextDate.getTime());
+        
+        JsonArray records = new Database().getTransactions(userId, currTime, nextTime);
+        
+        if (records.size() > 0) {
+            JsonObject dailyData = new JsonObject();
+            dailyData.add("data", records);
+            dailyData.addProperty("tanggal", sdf.format(current));
+            dailyData.addProperty("total", getDailyTotal(records));
+            return dailyData;
+        }
+        return null;
+    }
+    
+    private double getDailyTotal(JsonArray records) {
+        double total = 0;
+        for (JsonElement json : records) {
+            try {
+                JsonObject obj = (JsonObject) json;
+                
+                Double amount = obj.get("Amount").getAsDouble();
+                String type = obj.get("Type").getAsString();
+
+                if (type.equals("Income")) {
+                    total += amount;
+                } else if (type.equals("Expense")) {
+                    total -= amount;
+                }
+            } catch(Exception ex) {
+                logger.error(ex);
+            }
+        }
+        return total;
     }
 }
